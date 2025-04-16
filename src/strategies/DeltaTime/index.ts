6fb4312b6bfa39500f76e6aa9613e0b9dd7f87f3
Emil Widlund <hello@emilwidlund.com>
2025-04-16 09:52:43 +0200

    lint files

--- src/strategies/DeltaTime/index.ts
+++ src/strategies/DeltaTime/index.ts
@@ -2,7 +2,7 @@ import type { IngestionContext } from "../../ingestion";
 import { IngestionStrategy } from "../../strategy";
 
 type DeltaTimeStrategyContext = IngestionContext & {
-  deltaTime: number;
+	deltaTime: number;
 };
 
 type DeltaTimeStrategyNowResolver = () => number;
@@ -11,32 +11,32 @@ type DeltaTimeStrategyStart = () => DeltaTimeStrategyStop;
 type DeltaTimeStrategyStop = () => number;
 
 export class DeltaTimeStrategy extends IngestionStrategy<
-  DeltaTimeStrategyContext,
-  DeltaTimeStrategyStart
+	DeltaTimeStrategyContext,
+	DeltaTimeStrategyStart
 > {
-  public startTime = 0;
-  public endTime = 0;
-  public nowResolver: DeltaTimeStrategyNowResolver;
+	public startTime = 0;
+	public endTime = 0;
+	public nowResolver: DeltaTimeStrategyNowResolver;
 
-  constructor(now: DeltaTimeStrategyNowResolver) {
-    super();
+	constructor(now: DeltaTimeStrategyNowResolver) {
+		super();
 
-    this.nowResolver = now;
-  }
+		this.nowResolver = now;
+	}
 
-  public client(customerId: string): DeltaTimeStrategyStart {
-    return () => {
-      const startTime = this.nowResolver();
+	public client(customerId: string): DeltaTimeStrategyStart {
+		return () => {
+			const startTime = this.nowResolver();
 
-      return () => {
-        const endTime = this.nowResolver();
-        const deltaTime = endTime - startTime;
+			return () => {
+				const endTime = this.nowResolver();
+				const deltaTime = endTime - startTime;
 
-        const execute = this.createExecutionHandler();
-        execute({ deltaTime }, customerId);
+				const execute = this.createExecutionHandler();
+				execute({ deltaTime }, customerId);
 
-        return deltaTime;
-      };
-    };
-  }
+				return deltaTime;
+			};
+		};
+	}
 }
